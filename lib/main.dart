a112b669d041db58425694474d9cc90f066f9fb9
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-12 03:09:44 +0530

    12.03.2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -59,8 +60,71 @@ class _IntroScreenState extends State<IntroScreen> with TickerProviderStateMixin
                   )
               ),
               Expanded(
-
+                flex: 2,
+                child: Container(
+                  child: AvatarGlow(
+                    //endRadius: 140,
+                    duration: Duration(seconds: 2),
+                    glowColor: Colors.white,
+                    repeat: true,
+                    //repeatPauseDuration : Duration(seconds: 1),
+                    startDelay: Duration(seconds: 1),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          style: BorderStyle.none
+                        ),
+                        shape: BoxShape.circle
+                      ),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey.shade900,
+                        child: Container(
+                          child: Image.asset(
+                            'lib/images/tictactoelogo.png',
+                            color: Colors.white,
+                            fit: BoxFit.scaleDown,
+                          ),
+                        ),
+                        radius: 80.0,
+                      )
+                    ),
+                  ),
+                ),
               ),
+              Expanded(
+                  child: Padding(
+                      padding: const EdgeInsets.only(top: 80.0),
+                      child: Container(
+                        child: Text(
+                          "@ArnabC27",
+                          style: myNewFontWhite.copyWith(fontSize: 20),
+                        ),
+                      )
+                  )
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                },
+                child: Padding(
+                  padding: EdgeInsets.only(left: 40, right: 40, bottom: 60),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      color: Colors.white,
+                      child: Center(
+                        child: Text(
+                          "PLAY GAME",
+                          style: myNewFont,
+                        )
+                      )
+                    ),
+                  ),
+                ),
+              )
             ],
           )
         )
